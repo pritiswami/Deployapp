7a91ff0c2eef0b90c80af9d78c301c91bb9fd5b6
user <pritinswami@gmail.com>
2025-01-26 18:22:57 +0530

    updated code

--- src/main/java/com/example/DeployApp/DeployAppApplication.java
+++ src/main/java/com/example/DeployApp/DeployAppApplication.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class DeployAppApplication {
 
