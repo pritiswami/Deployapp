c61636ad6e9538edc7125fa95ed0b60f2d420117
user <pritinswami@gmail.com>
2025-01-26 16:21:17 +0530

    logger added

--- src/main/java/com/example/DeployApp/DeployAppApplication.java
+++ src/main/java/com/example/DeployApp/DeployAppApplication.java
@@ -1,5 +1,7 @@
 package com.example.DeployApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeployAppApplication {
 
+    Logger logger= LoggerFactory.getLogger(DeployAppApplication.class);
 
 	@GetMapping("hello")
 	public String helloWorld()
